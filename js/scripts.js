08d2819e8c8bc059e32159cf1d48511687d6689f
kevinboco <kevinalexanderboca@gmail.com>
2025-04-06 16:05:52 -0500

    subir proyecto

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+/*!
+* Start Bootstrap - Modern Business v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+let whatsappBtn = document.getElementById("whatsappBtn");
+    if (whatsappBtn) {
+        whatsappBtn.addEventListener("click", function() {
+            let phoneNumber = "573215116044"; // Reemplaza con tu número
+            let message = encodeURIComponent("¡Hola! Quiero más información.");
+            let url = `https://wa.me/${phoneNumber}?text=${message}`;
+            window.open(url, "_blank");
+        });
+    }
